48b3233311c26587921870e6a4d6688ea516f213
EC2 Default User <ec2-user@ip-172-31-9-120.ap-southeast-2.compute.internal>
2021-03-12 22:53:18 +0000

    terraform init

---
+++ backend.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = ">=0.12.0"
+  backend "s3" {
+    region  = "us-east-1"
+    profile = "default"
+    key     = "terraformstatefile"
+    bucket  = "yuanli-terraform-backend"
+  }
+
+
+
+
+
+
+}
